caa2bd26c05569089c78ffa9b5021b66de2474ff
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2024-01-29 00:43:47 -0300

    feat: api search

---
+++ script.js
@@ -0,0 +1,35 @@
+const searchInput = document.getElementById('search-input');
+const resultsArtist = document.getElementById('result-artist');
+const resultsPlaylist = document.getElementById('result_playlists');
+const artistName = document.getElementById('artist-name');
+const artistImage = document.getElementById('artist-img');
+
+function requestAPI(searchTerm){
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then(response => response.json())
+        .then(result => displayResults(result))
+}
+
+function displayResults(result){
+    resultsPlaylist.classList.add('hidden');
+    console.log('artistName', artistName);
+    console.log('artistImage', artistImage);
+    console.log('result', result);
+    result.forEach(artist => {
+        artistName.innerText = artist.name;
+        artistImage.src = artist.urlImg;
+    })
+    resultsArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    if(searchTerm === ''){
+        console.log('vazio')
+        resultsPlaylist.classList.remove('hidden');
+        resultsArtist.classList.add('hidden');
+        return;
+    }
+    requestAPI(searchTerm)
+})
